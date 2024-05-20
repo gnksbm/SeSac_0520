981d4a0706c0db346f3db799c5e71e3fdc6d6ba3
Geonseob Kim <gnksbm@gmail.com>
2024-05-20 11:03:33 +0900

    함수와 매개변수

--- SeSac_0520/ViewController.swift
+++ SeSac_0520/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var oneButton: UIButton!
+    @IBOutlet var twoButton: UIButton!
+    @IBOutlet var threeButton: UIButton!
+    
+    @IBOutlet var oneLabel: UILabel!
+    @IBOutlet var twoLabel: UILabel!
+    @IBOutlet var threeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        designLabelUI(labelName: oneLabel, thisIsTextColor: .red)
+        designLabelUI(labelName: twoLabel, thisIsTextColor: .yellow)
+        designLabelUI(labelName: threeLabel, thisIsTextColor: .green)
+        designButtonUI(oneButton)
+        designButtonUI(twoButton)
+        designButtonUI(threeButton)
     }
-
-
+    
+    func designLabelUI(
+        labelName: UILabel,
+        thisIsTextColor textColor: UIColor = .brown
+    ) {
+        labelName.text = "0"
+        labelName.textColor = textColor
+        labelName.font = .boldSystemFont(ofSize: 30)
+        labelName.textAlignment = .center
+    }
+    
+    func designButtonUI(
+        _ button: UIButton,
+        titleColor: UIColor = .red
+    ) {
+        button.setTitle("클릭", for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+    }
+    
+    @IBAction func oneButtonTapped(_ sender: UIButton) {
+        oneLabel.text = "1"
+    }
 }
-
